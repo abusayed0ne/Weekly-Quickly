afa2a8fc572af018c6c6a01f7ed21b2aa509c644
abusayed0ne <0ne.abusayed@gmail.com>
2024-09-01 02:03:21 +0600

    Update

---
+++ #1.js
@@ -0,0 +1,44 @@
+//Common Mistakes of JS Operator
+
+//1. Addition with Numbers: 
+const result1 = 5 + 2 ;
+console.log(result1);
+console.log(typeof result1);
+
+//2. Addition with a Number and a String:
+const result2 = 5 + "7" ;
+console.log(result2);
+console.log(typeof result2);
+
+//3. Multiple Operations 
+const result3 = 5 + 2 + "7";
+console.log(result3);
+console.log(typeof result3);
+
+//4. String Concatenation with Strings:
+const result4 =  "5" + "2";
+console.log(result4);
+console.log(typeof result4);
+
+//5. String Concatenation with Numbers and Strings:
+const result5 =  "5" + 2;
+console.log(result5);
+console.log(typeof result5);
+
+//6. Multiplication with Numbers and Strings:
+const result6 =  5 * "2";
+console.log(result6);
+console.log(typeof result6);
+
+//7. Division with Numbers and Strings:
+const result7 = "10" / 2;
+console.log(result7);
+console.log(typeof result7);
+
+//8. Comparison with Numbers and Strings:
+const result8 = 10 < "5";
+console.log(result8);
+console.log(typeof result8);
+ 
+
+
